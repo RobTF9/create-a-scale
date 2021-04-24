2812a270ed2216aa20b5d537b245556ea092d5bd
Rob Squires <rsquires@sans.org>
2021-04-24 07:38:17 +0100

    chore : update ci

--- dist/utils/createBreakpointScale.d.ts
+++
@@ -1,9 +0,0 @@
-declare function createBreakpointScale(breakpoint: number, size: number, scale: number): {
-    p: number;
-    xs: number;
-    s: number;
-    m: number;
-    l: number;
-    xl: number;
-};
-export default createBreakpointScale;
--- dist/utils/parseArguments.d.ts
+++
@@ -1,7 +0,0 @@
-declare function parseArguments(raw: string[]): {
-    size?: number;
-    scale?: number;
-    out: string;
-    breakpoints: number[] | boolean;
-};
-export default parseArguments;
